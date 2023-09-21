58b649950d760af742b2eb0fb60513eefb56083c
Cyber-Geooorge <george.cambodge.gmail.com>
2023-09-22 01:04:45 +0200

    matomo

---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useMatomo } from "@datapunt/matomo-tracker-react";
+
+const MyComponent = () => {
+  const { trackPageView, trackEvent } = useMatomo();
+
+  React.useEffect(() => {
+    trackPageView({});
+  }, []);
+
+  return (
+    <button onClick={() => trackEvent({ category: "button", action: "click" })}>
+      Cliquez sur moi
+    </button>
+  );
+};
